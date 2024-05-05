5b79e5360211575ed90b4cfd48495f0fa6840209
dianudi <144350056+dianudi@users.noreply.github.com>
2024-05-05 21:11:27 +0700

    fix footer year dynamic

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,16 +1,12 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import { AiFillGithub, AiOutlineTwitter, AiFillInstagram } from "react-icons/ai";
-import { FaLinkedinIn } from "react-icons/fa";
 
 function Footer() {
-  let date = new Date();
-  let year = date.getFullYear();
   return (
     <Container fluid className="footer">
       <Row>
         <Col md="4" className="footer-copywright">
-          <h3>Copyright © 2023 - {year} - Dianudi</h3>
+          <h3>Copyright © {new Date().getFullYear()} - Dianudi</h3>
         </Col>
       </Row>
     </Container>
