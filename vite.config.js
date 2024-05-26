1ea39b7d753c3f079267ad488a2e20b7e1ede7eb
Dian Udi <144350056+dianudi@users.noreply.github.com>
2024-05-26 16:09:21 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ export default defineConfig({
   // depending on your application, base can also be "/"
   base: "",
   plugins: [react()],
+  base: '/my-portfolio/',
   test: {
     // 👋 add the line below to add jsdom to vite
     environment: "jsdom",
