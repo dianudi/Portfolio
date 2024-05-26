4c78eb4644e4b718a5b378ddfe6fa72fec40baf8
Dian Udi <144350056+dianudi@users.noreply.github.com>
2024-05-26 16:21:08 +0700

    fix base url

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   // depending on your application, base can also be "/"
   plugins: [react()],
-  base: '/my-portfolio/',
+  base: '/Portfolio/',
   test: {
     // 👋 add the line below to add jsdom to vite
     environment: "jsdom",
